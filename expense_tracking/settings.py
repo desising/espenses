22764b784c871cca008763922df2b04da1370406
dsingh <deepak@monkfox.com>
2020-01-21 21:03:38 +0530

    Setting Static Root

--- expense_tracking/settings.py
+++ expense_tracking/settings.py
@@ -215,6 +215,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/expenses/my'
 LOGOUT_REDIRECT_URL = '/login'
 MEDIA_URL = '/media/'
